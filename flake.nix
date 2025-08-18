8a86c21e0af90faae73c2f847be7cde73df7d6e3
Braden Steffaniak <braden@Bradens-MacBook-Air.local>
2025-08-18 19:36:28 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # ghostty = {
+    #   url = "github:ghostty-org/ghostty";
+    # };
+};
+
+  outputs = inputs@{
+    self,
+    nix-darwin,
+    nixpkgs,
+    # ghostty
+  }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+          pkgs.neovim
+	  pkgs.tmux
+	  # ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+          pkgs.mkalias
+	  pkgs.lazygit
+	];
+
+	nixpkgs.config.allowUnfree = true;
+
+	system.activationScripts.applications.text = let
+	  env = pkgs.buildEnv {
+	    name = "system-applications";
+	    paths = config.environment.systemPackages;
+	    pathsToLink = "/Applications";
+	  };
+	in
+	  pkgs.lib.mkForce ''
+	  # Set up applications.
+	  echo "setting up /Applications..." >&2
+	  rm -rf /Applications/Nix\ Apps
+	  mkdir -p /Applications/Nix\ Apps
+	  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+	  while read -r src; do
+	    app_name=$(basename "$src")
+	    echo "copying $src" >&2
+	    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+	  done
+      '';
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Bradens-MacBook-Air
+    darwinConfigurations."Bradens-MacBook-Air" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
