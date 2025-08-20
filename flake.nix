aafef7092428d8bf62d0ce04cdfb067ef8c74995
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-08-20 19:20:51 -0400

    Fix formatting and add various sys packages

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
-};
+  };
 
   outputs = inputs@{
     self,
@@ -35,93 +35,100 @@
       environment.systemPackages =
         [ pkgs.vim
           pkgs.neovim
-	  pkgs.tmux
-	  # ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+          pkgs.tmux
+          # ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
           pkgs.mkalias
-	  pkgs.lazygit
-	];
+          pkgs.lazygit
+          pkgs.maven
+          pkgs.ripgrep
+          pkgs.fzf
+        ];
 
-	nixpkgs.config.allowUnfree = true;
+        nixpkgs.config.allowUnfree = true;
 
-	system.activationScripts.applications.text = let
-	  env = pkgs.buildEnv {
-	    name = "system-applications";
-	    paths = config.environment.systemPackages;
-	    pathsToLink = "/Applications";
-	  };
-	in
-	  pkgs.lib.mkForce ''
-	  # Set up applications.
-	  echo "setting up /Applications..." >&2
-	  rm -rf /Applications/Nix\ Apps
-	  mkdir -p /Applications/Nix\ Apps
-	  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	  while read -r src; do
-	    app_name=$(basename "$src")
-	    echo "copying $src" >&2
-	    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-	  done
-      '';
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      system.primaryUser = "braden";
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      homebrew = {
-        enable = true;
-	onActivation.cleanup = "zap";
-      };
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Bradens-MacBook-Air
-    darwinConfigurations."Bradens-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = true;
-
-            # User owning the Homebrew prefix
-            user = "braden";
-
-            # Optional: Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-            };
-
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            mutableTaps = false;
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
           };
-        }
-        # Optional: Align homebrew taps config with nix-homebrew
-        ({config, ...}: {
-          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
-        })
-      ];
-    };
-  };
+        in
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read -r src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
+
+        system.defaults = {
+          dock.autohide = true;
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        system.primaryUser = "braden";
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        homebrew = {
+          enable = true;
+          onActivation.cleanup = "zap";
+        };
+      };
+      in
+        {
+          # Build darwin flake using:
+          # $ darwin-rebuild build --flake .#Bradens-MacBook-Air
+          darwinConfigurations."Bradens-MacBook-Air" = nix-darwin.lib.darwinSystem {
+            modules = [
+              configuration
+              nix-homebrew.darwinModules.nix-homebrew
+              {
+                nix-homebrew = {
+                  # Install Homebrew under the default prefix
+                  enable = true;
+
+                  # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+                  enableRosetta = true;
+
+                  # User owning the Homebrew prefix
+                  user = "braden";
+
+                  # Optional: Declarative tap management
+                  taps = {
+                    "homebrew/homebrew-core" = homebrew-core;
+                    "homebrew/homebrew-cask" = homebrew-cask;
+                  };
+
+                  # Optional: Enable fully-declarative tap management
+                  #
+                  # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+                  mutableTaps = false;
+                };
+              }
+              # Optional: Align homebrew taps config with nix-homebrew
+              ({config, ...}: {
+                homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+              })
+            ];
+          };
+        };
 }
